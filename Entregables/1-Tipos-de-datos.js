ee09fa37f92f495ae3c5e207217105f273d384ad
Liss74 <lisstorave24@gmail.com>
2025-02-11 00:03:38 -0600

    style: :busts_in_silhouette: Agregué más ejemplos
    Agregué un ejemplo más de tipo número, dos de tipo stirng y además escribí un booleano mal para que se muestre cómo indefínido. Cabe mencionar que aquí se imprime que tipo de dato es, no se muestra la cantidad del número por ejemplo.

--- Entregables/1-Tipos-de-datos.js
+++ Entregables/1-Tipos-de-datos.js
@@ -4,12 +4,19 @@
 console.log(typeof 42);
 console.log(typeof -3.14);
 console.log(typeof 0);
+console.log(typeof -123e5);
 
 //sting
 console.log(typeof "Twenty20");
 console.log(typeof '123');
 console.log(typeof `true`);
+console.log(typeof "Veinticinco");
+console.log(typeof 'Undefinide');
 
 //boolean
 console.log(typeof true);
 console.log(typeof false);
+
+//boolean no escrito bien aparecerá como undefined	
+console.log(typeof True);
+console.log(typeof False);
