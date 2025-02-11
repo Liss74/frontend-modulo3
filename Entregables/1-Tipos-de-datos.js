3de46de7896180730af96b59877a484bb56e0f37
Liss74 <lisstorave24@gmail.com>
2025-02-10 23:52:32 -0600

    perf: :construction: Primeros ejemplos de tipos de datos
    Aquí pongo ejemplos de número, string y booleanos

---
+++ Entregables/1-Tipos-de-datos.js
@@ -0,0 +1,15 @@
+//  agrega el código para imprimir en la consola los distintos tipos de datos que existen en JS
+
+//número
+console.log(typeof 42);
+console.log(typeof -3.14);
+console.log(typeof 0);
+
+//sting
+console.log(typeof "Twenty20");
+console.log(typeof '123');
+console.log(typeof `true`);
+
+//boolean
+console.log(typeof true);
+console.log(typeof false);
