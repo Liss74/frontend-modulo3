71a93867ae42402bd58135405e6129c897c91b45
Liss74 <lisstorave24@gmail.com>
2025-02-18 14:05:00 -0600

    feat: :zap: Ejercicio de clase
    Realizó un ejercicio el profesor para determinar si habíamos entendido el tema

---
+++ Clase/Clase-06-02-2025/reto.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 3;
+
+if(a-b >3){
+    if(b-a == 2){
+
+    }else {
+        c = b*a;
+
+        if(c>15){
+            console.log("RES A");
+        }
+    }
+    console.log("RES C");
+} 
+ //Notemos que como la primera condición no se cumple no va a correr
