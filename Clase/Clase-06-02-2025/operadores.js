d92692b015298fc545076d4da1f10a16868cb138
Liss74 <lisstorave24@gmail.com>
2025-02-18 13:33:18 -0600

    chore: :hammer: Ejemplos de operadores
    Se creo una carpeta nueva que contenga dos archivos, cada uno tiene un tema diferente

---
+++ Clase/Clase-06-02-2025/operadores.js
@@ -0,0 +1,40 @@
+//Operador asignación
+let a= "a";
+
+//Matemáticos
+
+//suma
+let suma = 3 + 2;
+console.log(suma);
+
+//resta
+let resta = 3 - 2;
+console.log(resta);
+
+//multiplicación
+let multiplicacion = 3 * 2;
+console.log(multiplicacion);
+
+//división
+let division = 3 / 2;
+console.log(division);  
+
+//módulo
+let modulo = 3 % 2;
+console.log(modulo);
+
+//combinados
+
+//Incremento
+let i=0 ;
+i = i + 1;
+i += 1;
+i++;
+console.log(i);
+
+//Decremento
+let d=10 ;
+d = d - 1;
+d -= 1;
+d--;
+console.log(d);
