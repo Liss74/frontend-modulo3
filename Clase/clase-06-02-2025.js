bf3c29881db65920f5ac4469241fd372ace6dcb0
Liss74 <lisstorave24@gmail.com>
2025-02-18 13:14:34 -0600

    feat: Clase del jueves
    Ponemos las variables var, let y cons, también sus diferencias

---
+++ Clase/clase-06-02-2025.js
@@ -0,0 +1,18 @@
+var a = "palabra";
+var b = 123;
+var a = "otra cosa";
+//Lo que pasa con var es que lo que se encuentra a dentro se sustituye
+
+//----------------------
+let l ="esta";
+//Esto no va a funcionar, porque solo se puede declarar una vez let
+//let l = "esta otra";
+
+l ="esta otra"; //de esta forma si funciona, porque no se está declarando otra vez
+
+//----------------------
+const c ="constante";
+// const c = "variable"; Esta no funcionará
+//c = "variable"; Esta tampoco funcionará porque una constante no cambia
+
+console.log(l)
