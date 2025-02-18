2c1fdd7aabc90655c6841ce2a14f3ad89f9ceb95
Liss74 <lisstorave24@gmail.com>
2025-02-18 13:44:30 -0600

    chore: :sparkles: ejemplo de operadores condicionales
    Se creo un archivo nuevo junto con los diversos ejemplos y diferencia entre ellos

---
+++ Clase/Clase-06-02-2025/operadores_comparacion.js
@@ -0,0 +1,38 @@
+//Igualdad débil
+let a = "palabra";
+let b = "palabra";
+
+console.log(a == b); //true
+
+let x =0;
+let y = "0";
+
+console.log(x == y); //true
+
+//Igualdad fuerte
+
+console.log(a === b); //true
+consol.log(x === y); //false
+
+//Desigualdad débil
+
+console.log(a != b); //false
+
+//Desigualdad fuerte
+
+console.log(a !== b); //false
+
+//Mayor que
+let z = 5;
+let w = 3;
+
+console.log(z > w); //true
+
+//Menor que
+console.log(z < w); //false
+
+//Mayor o igual que
+console.log(z >= w); //true
+
+//Menor o igual que
+console.log(z <= w); //false
