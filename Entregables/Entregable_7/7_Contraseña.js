de39f2f456f79503a1c2b1407d7861368f50ad9d
Liss74 <lisstorave24@gmail.com>
2025-03-06 00:15:56 -0600

    feat: :construction: Subo la parte visual del entregable 7
    Primera parte del entregable 7, tome captura de la imagen mostrada en el curso y le pedí a la IA que me hiciera una similar, solo modifiqué nombres y visualización de unas cosas
    
    Empiezo a trabajar en js para su funcionamiento

---
+++ Entregables/Entregable_7/7_Contraseña.js
@@ -0,0 +1,6 @@
+/* Objetivo
+El objetivo es crear un sitio que nos permita generar una contraseña segura
+Apartir de la imagen propocionada maqueta y genera el formulario necesario para generar el html
+El sitio debe ser capaza de generar una contraseña diferente de mas de 8 palabras usando simbolos, letras y numéros
+*/
+
