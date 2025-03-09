447eeb781befcb12368958e764a9e494aab397bf
Liss74 <lisstorave24@gmail.com>
2025-03-08 23:59:07 -0600

    perf: :building_construction: Agrego el funcionamiento de la tarea 7
    Introduzco cosas en específico para generar la contraseña y al momento de irlo escribiendo se activa o desactiva la casilla del checkbox

--- Entregables/Entregable_7/7_Contraseña.js
+++ Entregables/Entregable_7/7_Contraseña.js
@@ -4,3 +4,22 @@ Apartir de la imagen propocionada maqueta y genera el formulario necesario para 
 El sitio debe ser capaza de generar una contraseña diferente de mas de 8 palabras usando simbolos, letras y numéros
 */
 
+let contraseña= [];
+let botonGenerar = document.getElementById("boton");
+
+
+//confirmamos si la contraseña tiene los requisitos
+function verificacionContraseña(){
+    let texto = document.getElementById("texto").value;
+
+    document.getElementById("caracteres").checked = texto.length >= 8;
+    document.getElementById("mayuscula").checked = /[A-Z]/.test(texto);
+    document.getElementById("minuscula").checked = /[a-z]/.test(texto);
+    document.getElementById("numeros").checked = /[0-9]/.test(texto);
+    document.getElementById("simbolo").checked = /[*#@]/.test(texto);
+}
+
+function guardarContraseña(){
+    let texto = document.getElementById("texto").value;
+    contraseña.push(texto)
+}
