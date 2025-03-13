a828553ff24aefc5aecf2937c0d061084e75d0a3
Liss74 <lisstorave24@gmail.com>
2025-03-12 19:37:35 -0600

    build: :bug: Corrigo detalles de la tarea 1
    Corregí detalles qué dejé pasar, esta observación generada por mi sensei, las cuales fueron: En la línea 14, veo que colocaste undefined dentro de comillas. Sin embargo, undefined es un tipo primitivo y no debería ir como string. Además, en las líneas 21 y 22, los valores booleanos (true y false) deben escribirse en minúsculas, ya que en mayúsculas pueden generar errores. Por favor, corrige estos detalles para que el código siga los estándares correctos.

--- Entregables/1-Tipos-de-datos.js
+++ Entregables/1-Tipos-de-datos.js
@@ -11,12 +11,13 @@ console.log(typeof "Twenty20");
 console.log(typeof '123');
 console.log(typeof `true`);
 console.log(typeof "Veinticinco");
-console.log(typeof 'Undefinide');
+console.log(typeof Undefinide); //undefined es un tipo primitivo y no debería ir como string
 
 //boolean
 console.log(typeof true);
 console.log(typeof false);
 
 //boolean no escrito bien aparecerá como undefined	
-console.log(typeof True);
-console.log(typeof False);
+console.log(typeof true);
+console.log(typeof false);
+//los valores booleanos (true y false) deben escribirse en minúsculas, ya que en mayúsculas pueden generar errores
